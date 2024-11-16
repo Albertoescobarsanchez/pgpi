daa7b3e79ad3953a5c0c0603848287673b8d3156
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-11-16 17:54:40 +0100

    add login and register template

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 from app.frontend import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('register/',views.register, name='register')
 ]
