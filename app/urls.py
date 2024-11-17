52571f8f5b7358b29f07f4a9f334361fb67e5868
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-11-17 17:56:22 +0100

    add images correctly

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from app.frontend import views
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('editarUsuario/', views.editarUsuario, name='editarUsuario')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
