54aea2ee7d30b57ac395078e61d942325216dfce
Manuel Orta <manortper1@alum.us.es>
2024-11-17 16:31:00 +0100

    añadir cesta de compra

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('register/',views.register, name='register')
+    path('register/',views.register, name='register'),
+    path('cesta/', views.cesta, name='cesta')
 ]
