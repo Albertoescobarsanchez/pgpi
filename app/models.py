9306d7235e5a055b2f167518b6bc0bcdc1d8821a
Alberto <albescsan1@alum.us.es>
2024-11-23 13:32:36 +0100

    entidades cesta

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100) 
@@ -8,3 +8,11 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField()           
     categoria = models.CharField(max_length=100)
+
+class Cesta(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+class CestaProducto(models.Model):
+    cesta = models.ForeignKey(Cesta, related_name='productos', on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
