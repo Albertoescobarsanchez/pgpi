4c528000c29b456a7f4c10dbce4585204b6b3c25
Alberto <albescsan1@alum.us.es>
2024-11-23 13:35:09 +0100

    cambio entidad cesta

--- app/models.py
+++ app/models.py
@@ -7,12 +7,9 @@ class Producto(models.Model):
     imagen = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField()           
-    categoria = models.CharField(max_length=100)
-
-class Cesta(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    categoria = models.CharField(max_length=100)  
 
 class CestaProducto(models.Model):
-    cesta = models.ForeignKey(Cesta, related_name='productos', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField(default=1)
