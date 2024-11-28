f26466143f995209cd776d703784f47c319d1a3a
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-11-28 21:56:49 +0100

    añadir cabeceras a las vistas

--- app/views.py
+++ app/views.py
@@ -10,6 +10,8 @@ from .models import Pedido, Producto, ProductoPedido
 from .models import ProductoCesta
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
+from .models import Pedido
 
 
 def home(request):
@@ -239,8 +241,6 @@ def eliminar_producto_no_autenticado(request, producto_id):
 
     return redirect('ver_cesta')
 
-from django.shortcuts import render
-from .models import Pedido
 
 def pedidos(request):
     pedidos_info = []
